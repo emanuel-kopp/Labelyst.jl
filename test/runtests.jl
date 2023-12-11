d79f9ec2cdc641011156bb38220218d9b3143e17
emanuel-kopp <emanuel.kopp@outlook.com>
2023-12-11 13:35:39 +0100

    Added some basic test fotr make_outfile

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,16 @@
 using Labelyst
 using Test
+using DataFrames
 
-@testset "Labelyst.jl" begin
-    # Write your tests here.
+@testset "Assertion Tests" begin
+    # Test make_outfile
+    testfile = "/not/existing/dir"
+    @test_throws AssertionError Labelyst.make_outfile(testfile)
+end
+
+@testset "make_outfile" begin
+    # Test make_outfile
+    testfile = "testdir"
+    @test Labelyst.make_outfile(testfile) == "testdir.typ"
+    rm("testdir.typ")
 end
