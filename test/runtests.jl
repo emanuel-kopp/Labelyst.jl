36f4d9df8949ad925cc5995c0222bf86cb3007f1
emanuel-kopp <emanuel.kopp@outlook.com>
2023-12-11 13:51:18 +0100

    Added paper size check

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,9 @@ using DataFrames
 @testset "Assertion Tests" begin
     # Test make_outfile
     testfile = "/not/existing/dir"
+    df = DataFrame(ID = ["p1"], label = ["lab1"])
     @test_throws AssertionError Labelyst.make_outfile(testfile)
+    @test_throws AssertionError labelyst(df, testfile, "z6", [3, 2])
 end
 
 @testset "make_outfile" begin
@@ -14,3 +16,4 @@ end
     @test Labelyst.make_outfile(testfile) == "testdir.typ"
     rm("testdir.typ")
 end
+
