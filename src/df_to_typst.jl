42983bcd169fae61c1981cd3f9e78b63b8db3234
emanuel-kopp <emanuel.kopp@outlook.com>
2023-12-19 16:02:58 +0100

    Shorter examples in readme

--- src/df_to_typst.jl
+++ src/df_to_typst.jl
@@ -33,14 +33,27 @@ end
 
 # Method 1: assumes one label per page
 """
-    labelyst(dataframe, output_file; <keyword arguments>)
+    labelyst(dataframe, output_file, paper_format::Vector{String}; <kwargs>)
+    labelyst(dataframe, output_file, paper_format::String; <kwargs>)
+    labelyst(dataframe, output_file, paper_format::Vector{String}; <kwargs>)
 
 Take a `julia DataFrame` and produce a `.pdf` file with labels containing QR-codes and human readable codes.
 
-...
 ### Keyword arguments
 - `font_size::String`: sets the font size for all the text on the label, default is `"12pt"`
 - `make_pdf::Bool`: define wether you want a `.pdf` as output or a raw `.typ` file, deault is `true`
+
+# Examples
+```julia
+    # labels of size 90mm X 17mm, one per page, saved as "pot_labels.pdf"
+    labelyst(dataframe, "pot_labels", ["90mm", "17mm"])
+
+    # DIN A4 pages with 30 labels (10 rows x 3 cols), saved as "adh_labels.pdf"
+    labelyst(dataframe, "adh_labels", "a4", [10, 3])
+
+    # custom size pages with 50 labels per page (10 rows x 5 cols), saved as "cust_lab.pdf"
+    labelyst(dataframe, "cust_lab", ["178mm", "250mm"], [10, 5]; font_size = "8pt") 
+```
 """
 function labelyst(dataframe, output_file, paper_format::Vector{String}; font_size="12pt", make_pdf = true)
 
