34de3ed457131a24036cb0c197d1c16f3a70d6f0
emanuel-kopp <emanuel.kopp@outlook.com>
2024-01-09 18:49:38 +0100

    deploying documentation

--- docs/make.jl
+++ docs/make.jl
@@ -13,6 +13,6 @@ makedocs(
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-deploydocs(;
+deploydocs(
     repo="github.com/emanuel-kopp/Labelyst.jl"
 )
