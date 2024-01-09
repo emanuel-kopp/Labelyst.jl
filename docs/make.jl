27ddfa9a5d0c7c68f9b50504f084ca43249ce628
emanuel-kopp <emanuel.kopp@outlook.com>
2024-01-09 18:38:59 +0100

    deploying documentation

--- docs/make.jl
+++ docs/make.jl
@@ -14,5 +14,5 @@ makedocs(
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
 deploydocs(;
-    repo="github.com/emanuel-kopp/Labelyst.jl.git"
+    repo="github.com/emanuel-kopp/Labelyst.jl"
 )
